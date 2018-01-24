7edd2330b6a4afbc3dc50d5cfcc256d439c62531
syen <207024883@qq.com>
2018-01-24 14:37:10 +0800

    添加路由模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
+import { NgHeaderComponent } from './ng-header/ng-header.component';
 
+import { IndexRouter } from '../router/index-router';
+import { IntelligentAnalysisComponent } from './intelligent-analysis/intelligent-analysis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgHeaderComponent,
+    IntelligentAnalysisComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IndexRouter,
+    NgZorroAntdModule.forRoot()
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
